cabc3d18a1645c9e066299ced85360cf3b5541bb
Maxim Potekhin <potekhin@bnl.gov>
2025-07-25 13:20:25 -0400

    A stub for the rucio related code.

---
+++ data_agent/data_agent.py
@@ -0,0 +1,10 @@
+from rucio.client import Client
+from rucio.client.uploadclient import UploadClient
+from rucio.common.exception import RucioException
+from rucio.common.utils import generate_uuid
+from rucio.common.types import InternalAccount
+from rucio.common.logging import setup_logging
+import logging
+import os
+from typing import List, Dict, Any
+from rucio.common.utils import parse_replicas
